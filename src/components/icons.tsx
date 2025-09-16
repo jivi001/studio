52221205222d263a4a248124f711b9ad4364d439
Jivitesh <jiviteshgd28@gmail.com>
2025-09-16 12:12:14 +0000

    make this as logo

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,4 +1,5 @@
 import { cn } from '@/lib/utils';
+import { GraduationCap, Bell } from 'lucide-react';
 import type { SVGProps } from 'react';
 
 export function Logo(props: SVGProps<SVGSVGElement>) {
@@ -14,8 +15,13 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       className={cn('h-6 w-6', props.className)}
       {...props}
     >
-      <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
-      <polyline points="22 4 12 14.01 9 11.01" />
+      <title>Attendance Monitor Logo</title>
+      {/* Bell shape */}
+      <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
+      <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
+      {/* Graduation Cap shape */}
+      <path d="m22 10-10 4-10-4" />
+      <path d="M6 12v5c0 .55.45 1 1 1h12c.55 0 1-.45 1-1v-5" />
     </svg>
   );
 }
