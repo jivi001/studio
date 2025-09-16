b8f384239b41b4756b089bdb88eab284aff1b3e3
Jivitesh <jiviteshgd28@gmail.com>
2025-09-16 11:13:11 +0000

    invert the row into coloum (_for element div_)

--- src/components/timetable.tsx
+++ src/components/timetable.tsx
@@ -1,48 +1,89 @@
 // src/components/timetable.tsx
 import {
-    Table,
-    TableBody,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-  } from "@/components/ui/table"
-  
-  const timetableData = [
-    { time: "09:00 - 10:00", mon: "Class 10 - Maths", tue: "Class 9 - Science", wed: "Class 10 - Maths", thu: "Free", fri: "Class 8 - Maths" },
-    { time: "10:00 - 11:00", mon: "Class 9 - Science", tue: "Class 10 - Maths", wed: "Class 9 - Science", thu: "Class 8 - Maths", fri: "Free" },
-    { time: "11:00 - 12:00", mon: "Free", tue: "Class 8 - Maths", wed: "Free", thu: "Class 10 - Maths", fri: "Class 9 - Science" },
-    { time: "12:00 - 01:00", mon: "Lunch", tue: "Lunch", wed: "Lunch", thu: "Lunch", fri: "Lunch" },
-    { time: "01:00 - 02:00", mon: "Class 7 - Science", tue: "Free", wed: "Class 6 - Maths", thu: "Class 7 - Science", fri: "Class 6 - Maths" },
-    { time: "02:00 - 03:00", mon: "Class 6 - Maths", tue: "Class 7 - Science", fri: "Free", wed: "Class 7 - Science", thu: "Class 6 - Maths" },
-  ];
-  
-  export function Timetable() {
-    return (
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead className="w-[150px]">Time</TableHead>
-            <TableHead>Monday</TableHead>
-            <TableHead>Tuesday</TableHead>
-            <TableHead>Wednesday</TableHead>
-            <TableHead>Thursday</TableHead>
-            <TableHead>Friday</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {timetableData.map((row) => (
-            <TableRow key={row.time}>
-              <TableCell className="font-medium">{row.time}</TableCell>
-              <TableCell>{row.mon}</TableCell>
-              <TableCell>{row.tue}</TableCell>
-              <TableCell>{row.wed}</TableCell>
-              <TableCell>{row.thu}</TableCell>
-              <TableCell>{row.fri}</TableCell>
-            </TableRow>
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+const timeSlots = [
+  "09:00 - 10:00",
+  "10:00 - 11:00",
+  "11:00 - 12:00",
+  "12:00 - 01:00",
+  "01:00 - 02:00",
+  "02:00 - 03:00",
+];
+
+const scheduleData: { [key: string]: { [key: string]: string } } = {
+  "09:00 - 10:00": {
+    Monday: "Class 10 - Maths",
+    Tuesday: "Class 9 - Science",
+    Wednesday: "Class 10 - Maths",
+    Thursday: "Free",
+    Friday: "Class 8 - Maths",
+  },
+  "10:00 - 11:00": {
+    Monday: "Class 9 - Science",
+    Tuesday: "Class 10 - Maths",
+    Wednesday: "Class 9 - Science",
+    Thursday: "Class 8 - Maths",
+    Friday: "Free",
+  },
+  "11:00 - 12:00": {
+    Monday: "Free",
+    Tuesday: "Class 8 - Maths",
+    Wednesday: "Free",
+    Thursday: "Class 10 - Maths",
+    Friday: "Class 9 - Science",
+  },
+  "12:00 - 01:00": {
+    Monday: "Lunch",
+    Tuesday: "Lunch",
+    Wednesday: "Lunch",
+    Thursday: "Lunch",
+    Friday: "Lunch",
+  },
+  "01:00 - 02:00": {
+    Monday: "Class 7 - Science",
+    Tuesday: "Free",
+    Wednesday: "Class 6 - Maths",
+    Thursday: "Class 7 - Science",
+    Friday: "Class 6 - Maths",
+  },
+  "02:00 - 03:00": {
+    Monday: "Class 6 - Maths",
+    Tuesday: "Class 7 - Science",
+    Wednesday: "Class 7 - Science",
+    Thursday: "Class 6 - Maths",
+    Friday: "Free",
+  },
+};
+
+export function Timetable() {
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead className="w-[150px]">Day</TableHead>
+          {timeSlots.map((time) => (
+            <TableHead key={time}>{time}</TableHead>
           ))}
-        </TableBody>
-      </Table>
-    )
-  }
-  
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {days.map((day) => (
+          <TableRow key={day}>
+            <TableCell className="font-medium">{day}</TableCell>
+            {timeSlots.map((time) => (
+              <TableCell key={time}>{scheduleData[time]?.[day] || 'Free'}</TableCell>
+            ))}
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
